05301b61f01452311f918d99bfdac3a0e530d01d
Harry Apriadi <harryapriadi1@gmail.com>
2019-10-11 00:10:44 +0700

    PBOULANG-10116413-Latihan18-Kelipatan

---
+++ src/kelipatan/Kelipatan.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kelipatan;
+
+/**
+ *
+ * @author Lenovo
+ * NAMA         : HARRY APRIADI
+ * KELAS        : PBONGULANG
+ * NIM          : 10116413
+ * DESKRIPSI    : Berisi Program untuk menghitung kelipatan
+ */
+public class Kelipatan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        for (double i=3.5; i <= 35; i = i + 3.5) {
+            System.out.println(i);
+    }
+    }
+    
+}
